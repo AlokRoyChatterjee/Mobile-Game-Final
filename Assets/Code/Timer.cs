3b08b981bb7e47e7266bc81806ba05c918638a5e
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-11-26 19:56:06 -0500

    Add files via upload

---
+++ Assets/Code/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Timer : MonoBehaviour
+{
+    public Text timestring; 
+    public Text score;
+    public float secondsnow = 120f;
+    public float otherseconds=128f;
+    void Update()
+    {
+        secondsnow -= Time.deltaTime;
+        otherseconds-=Time.deltaTime;
+        timestring.text = "Game Timer: "+(secondsnow).ToString("00")+" seconds left";
+        if (secondsnow < 0)
+        {
+            if(int.Parse(score.text)<10){
+                timestring.text="Game Over: Your score has no medal. This level is restarting now...";
+            }
+            else if(int.Parse(score.text)>=10 && int.Parse(score.text)<20){
+            timestring.text="Game Over: Your score is a bronze medal. This level is restarting now...";
+            }
+            else if(int.Parse(score.text)>=20 && int.Parse(score.text)<35){
+                timestring.text="Game Over: Your score is a silver medal. This level is restarting now...";
+            }
+            else if(int.Parse(score.text)>=35){
+                timestring.text="Game Over: Your score is a gold medal. This level is restarting now...";
+            }
+        }
+        if(otherseconds<0){
+            SceneManager.LoadScene("Easy Level");
+        }
+
+        
+    }
+}
